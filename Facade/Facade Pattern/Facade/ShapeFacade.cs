53f207e50b41ab24a9e51b336b7f7726be2549cc
10604308 <Yishan_Chen@wistron.com>
2019-06-27 16:54:32 +0800

    Facade Pattern

---
+++ Facade/Facade Pattern/Facade/ShapeFacade.cs
@@ -0,0 +1,42 @@
+﻿using Facade_Pattern.Subsystems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade_Pattern.Facade
+{
+    public class ShapeFacade
+    {
+        private IShape circle;
+        private IShape rectangle;
+        private IShape square;
+
+        public ShapeFacade()
+        {
+            circle = new Circle();
+            rectangle = new Rectangle();
+            square = new Square();
+        }
+
+        public void DrawCircle()
+        {
+            circle.Draw();
+        }
+        public void DrawRectangle()
+        {
+            rectangle.Draw();
+        }
+        public void DrawSquare()
+        {
+            square.Draw();
+        }
+        public void DrawAll()
+        {
+            circle.Draw();
+            rectangle.Draw();
+            square.Draw();
+        }
+    }
+}
---
+++ Facade/Facade Pattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using Facade_Pattern.Facade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShapeFacade shapeFacade = new ShapeFacade();
+            shapeFacade.DrawAll();
+
+            Console.ReadLine();
+        }
+    }
+}
